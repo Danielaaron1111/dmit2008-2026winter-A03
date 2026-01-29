2012753a869941dfd4332f99abc716c7d421f55e
Oliver Antoniu <oliver+github@abolish.golf>
2026-01-29 09:16:49 -0700

    rev [02 - reactive patterns]: ex01 - pubsub: Step 2+3: inject generic PubSub functionality into expenses-specific handler

--- 02 -reactive patterns/examples/01 - expense tracker, pub-sub pattern/instructor/expenses.js
+++ 02 -reactive patterns/examples/01 - expense tracker, pub-sub pattern/instructor/expenses.js
@@ -4,6 +4,7 @@
 // Note: We don't need to hardcode for specific event types;
 // We can just make a generic 'mailman' and then wrap it in a more specific case later.
 
+// Step 1: a simple pub-sub broker
 const PubSub = {
     _subscribers: {}, // store who is subscribed, and to what event
     // {
@@ -28,3 +29,43 @@ const PubSub = {
         }
     },
 };
+
+
+
+
+// Step 2: Expenses Object Literal with Methods.
+// We'll be 'borrowing' functionality from the generic PubSub handler in the next step -
+// hence the use of e.g. this.publish(), which we don't have to redefine in this object.
+const expenses = {
+  list: [],
+
+  addExpense(...exp) {
+    this.list.push(...exp);
+    this.publish("update", this.list);
+  },
+
+  filterExpense(input) {
+    const result = this.list.filter(exp => {
+        if(exp.title.toLowerCase().includes(input.toLowerCase()) ||
+           exp.category.toLowerCase().includes(input.toLowerCase()) ||
+           exp.date.toLowerCase().includes(input.toLowerCase()) ||
+           exp.amount.toString().toLowerCase().includes(input.toLowerCase())) {
+            return true;
+        }
+    });
+    this.publish("update", result);
+  },
+
+  clear() {
+    this.list = [];
+    this.publish("update", this.list);
+  }
+};
+
+// Step 3: Add PubSub functionality
+// Object.assign copies properties and functionality
+// to a target object (param1: expenses) from a source object (param2: PubSub).
+// see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+Object.assign(expenses, PubSub);
+
+export default expenses;
