afe7a02ea2789fcedbdce370b80e57d8a689b7e6
oaao <oliver+github@abolish.golf>
2026-03-30 09:39:29 -0600

    add [07 - routing]: ex - simple Links

--- 07 - routing/example - spaaaace/instructor/components/NavBar.js
+++ 07 - routing/example - spaaaace/instructor/components/NavBar.js
@@ -1,3 +1,7 @@
+// next.js routing components
+import Link from 'next/link';
+
+// mui components
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography'
@@ -6,11 +10,17 @@ import Typography from '@mui/material/Typography'
 export default function NavBar(props) {
   return <AppBar position="static">
     <Toolbar>
+
       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Link href="/">
           Space Agency App
+        </Link>
       </Typography>
+
       <Typography variant="h6" component="div" >
-        About
+        <Link href="/about">
+          About
+        </Link>
       </Typography>
     </Toolbar>
   </AppBar>
